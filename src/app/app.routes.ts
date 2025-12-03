1090586efec2ec5b8a453075e01465731ea93005
obrioxia <j.obrien@obrioxia.com>
2025-12-03 10:05:49 +0000

    Fix AuthGuard import casing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { HubComponent } from './pages/hub/hub.component';
 import { LoggerComponent } from './pages/hub/logger/logger.component';
@@ -8,10 +8,10 @@ import { ShredderComponent } from './pages/hub/shredder/shredder.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'hub', component: HubComponent, canActivate: [AuthGuard] },
-  { path: 'hub/logger', component: LoggerComponent, canActivate: [AuthGuard] },
-  { path: 'hub/verifier', component: VerifierComponent, canActivate: [AuthGuard] },
-  { path: 'hub/shredder', component: ShredderComponent, canActivate: [AuthGuard] },
+  { path: 'hub', component: HubComponent, canActivate: [authGuard] },
+  { path: 'hub/logger', component: LoggerComponent, canActivate: [authGuard] },
+  { path: 'hub/verifier', component: VerifierComponent, canActivate: [authGuard] },
+  { path: 'hub/shredder', component: ShredderComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
 
